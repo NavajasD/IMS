9563bea87b13224e856d3b0e3059f44a2db2eef0
Daniel Navajas <daniel.navajas@globallogic.com>
2023-03-23 18:44:29 -0300

    added view inventories by name use case mvp

---
+++ IMS.Plugins/IMS.Plugins.InMemory/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using IMS.UseCases.PluginInterfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Plugins.InMemory
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddSingleton<IInventoryRepository, InventoryRepository>();
+            return services;
+        }
+    }
+}
