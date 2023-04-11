61b148e3de8d5b769908bf988c3b3d86654cc59d
Daniel Navajas <104368210+NavajasD@users.noreply.github.com>
2023-04-11 16:01:30 -0300

    Feature/edit inventory (#3)
    * moved components into folder
    
    * added EditInventoryUseCase and interface
    
    * added EditInventory Page
    
    * finished edirt inventory page and feature
    
    ---------
    
    Co-authored-by: Daniel Navajas <daniel.navajas@globallogic.com>

--- IMS.Plugins/IMS.Plugins.InMemory/InventoryRepository.cs
+++ IMS.Plugins/IMS.Plugins.InMemory/InventoryRepository.cs
@@ -34,5 +34,34 @@ namespace IMS.Plugins.InMemory
 
             return _inventories.Where(x => x.InventoryName.Contains(name, StringComparison.OrdinalIgnoreCase));
         }
+
+        public async Task<Inventory> GetInventoryByIdAsync(int inventoryId)
+        {
+            var inv = _inventories.First(x => x.InventoryId == inventoryId);
+            var newInv = new Inventory()
+            {
+                InventoryId = inv.InventoryId,
+                InventoryName = inv.InventoryName,
+                Quantity = inv.Quantity,
+                Price = inv.Price
+            };
+            return await Task.FromResult(newInv);
+        }
+
+        public Task UpdateInventoryAsync(Inventory inventory)
+        {
+            if(_inventories.Any(x => x.InventoryId != inventory.InventoryId &&
+            x.InventoryName.Equals(inventory.InventoryName, StringComparison.OrdinalIgnoreCase)))
+                return Task.CompletedTask;
+
+            var inv = _inventories.FirstOrDefault(x => x.InventoryId == inventory.InventoryId);
+            if (inv != null)
+            {
+                inv.InventoryName = inventory.InventoryName;
+                inv.Price = inventory.Price;
+                inv.Quantity = inventory.Quantity;
+            }
+            return Task.CompletedTask;
+        }
     }
 }
--- IMS.UseCases/DependencyInjection.cs
+++ IMS.UseCases/DependencyInjection.cs
@@ -15,6 +15,9 @@ namespace IMS.UseCases
         {
             services.AddTransient<IViewInventoriesByNameUseCase, ViewInventoriesByNameUseCase>();
             services.AddTransient<IAddInventoryUseCase, AddInventoryUseCase>();
+            services.AddTransient<IEditInventoryUseCase, EditInventoryUseCase>();
+            services.AddTransient<IViewInventoryByIdUseCase, ViewInventoryByIdUseCase>();
+
             return services;
         }
     }
