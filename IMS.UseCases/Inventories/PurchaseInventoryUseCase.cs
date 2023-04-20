cf3d7058cf6dc9653a128bb3b144f10baf09f0f2
Daniel Navajas <daniel.navajas@globallogic.com>
2023-04-20 18:20:12 -0300

    added purchase inventory use case

---
+++ IMS.UseCases/Inventories/PurchaseInventoryUseCase.cs
@@ -0,0 +1,32 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.UseCases.Inventories
+{
+    public class PurchaseInventoryUseCase
+    {
+        private readonly IInventoryRepository inventoryRepository;
+        private readonly IInventoryTransactionRepository inventoryTransactionRepository;
+
+        public PurchaseInventoryUseCase(IInventoryRepository inventoryRepository, IInventoryTransactionRepository inventoryTransactionRepository)
+        {
+            this.inventoryRepository = inventoryRepository;
+            this.inventoryTransactionRepository = inventoryTransactionRepository;
+        }
+
+        public async Task ExecuteAsync(string poNumber, Inventory inventory, int quantity, string doneBy)
+        {
+            //insert a record in the transaction table
+            await inventoryTransactionRepository.PurchaseAsync(poNumber, inventory, quantity, doneBy, inventory.Price);
+
+            //increase the quantity
+            inventory.Quantity += quantity;
+            await inventoryRepository.UpdateInventoryAsync(inventory);
+        }
+    }
+}
