8baeca4b5162819355ac4eae764c0e045ea2dda1
Daniel Navajas <daniel.navajas@globallogic.com>
2023-04-20 18:40:34 -0300

    Added InMemory InventoryTransactionRepository

---
+++ IMS.Plugins/IMS.Plugins.InMemory/InventoryTransactionRepository.cs
@@ -0,0 +1,30 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Plugins.InMemory
+{
+    public class InventoryTransactionRepository : IInventoryTransactionRepository
+    {
+        public List<InventoryTransaction> _inventoryTransactions = new();
+        public Task PurchaseAsync(string poNumber, Inventory inventory, int quantity, string doneBy, double price)
+        {
+            _inventoryTransactions.Add(new()
+            {
+                PoNumber = poNumber,
+                InventoryId = inventory.InventoryId,
+                ActivityType = InventoryTransactionType.PurchaseInventory,
+                QuantityBefore = inventory.Quantity,
+                QuantityAfter = inventory.Quantity + quantity,
+                UnitPrice = price,
+                TransactionDate = DateTime.UtcNow,
+                DoneBy = doneBy
+            });
+            return Task.CompletedTask;
+        }
+    }
+}
