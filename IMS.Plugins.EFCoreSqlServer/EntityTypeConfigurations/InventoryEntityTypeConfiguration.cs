f98ad67a4f38195b83bcfaf44715fbadd55ae7e3
Daniel Navajas <104368210+NavajasD@users.noreply.github.com>
2023-06-28 16:51:05 -0300

    added EFCore project, db context and EntityTypeConfigurations

---
+++ IMS.Plugins.EFCoreSqlServer/EntityTypeConfigurations/InventoryEntityTypeConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using IMS.CoreBusiness;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Plugins.EFCoreSqlServer.EntityTypeConfigurations
+{
+    public class InventoryEntityTypeConfiguration : IEntityTypeConfiguration<Inventory>
+    {
+        public void Configure(EntityTypeBuilder<Inventory> builder)
+        {
+            builder
+                .HasKey(x => x.InventoryId);
+
+            builder
+                .HasMany(x => x.ProductInventories)
+                .WithOne(x => x.Inventory)
+                .HasForeignKey(x => x.InventoryId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder
+                .HasMany(x => x.InventoryTransactions)
+                .WithOne(x => x.Inventory)
+                .HasForeignKey(x => x.InventoryId);
+        }
+    }
+}
---
+++ IMS.Plugins.EFCoreSqlServer/EntityTypeConfigurations/ProductInventoryEntityTypeConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using IMS.CoreBusiness;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Plugins.EFCoreSqlServer.EntityTypeConfigurations
+{
+    public class ProductInventoryEntityTypeConfiguration : IEntityTypeConfiguration<ProductInventory>
+    {
+        public void Configure(EntityTypeBuilder<ProductInventory> builder)
+        {
+            builder
+                .HasKey(x => new {x.InventoryId, x.ProductId});
+
+            builder
+                .HasOne(x => x.Product)
+                .WithMany(x => x.ProductInventories)
+                .HasForeignKey(x => x.ProductId);
+
+            builder
+                .HasOne(x => x.Inventory)
+                .WithMany(x => x.ProductInventories)
+                .HasForeignKey(x => x.InventoryId);
+
+        }
+    }
+}
